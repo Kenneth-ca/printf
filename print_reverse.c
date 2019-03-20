823da43017f6c708541bd98f98463e043705e5e5
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-03-19 22:04:25 -0500

    Fixing BettyQA

--- print_reverse.c
+++ print_reverse.c
@@ -7,7 +7,7 @@
  * _print_rev_recursion - Prints a string in reverse, followed by a new line.
  * @s: the string to be printed
  * @length: String Length to be calculated.
- * 
+ *
  * Return: String Length.
  */
 int _print_rev_recursion(char *s, int length)
